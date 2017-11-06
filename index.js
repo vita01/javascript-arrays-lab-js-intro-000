f9d798c0ce460b9cc48316e4b61ce9b32b551f7d
Victoria <vitalina27@gmail.com>
2017-11-06 12:04:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendKitten(name){
+  var arr=kittens.push(name);
+  return kittens;
+}
+destructivelyPrependKitten(name){
+kittens.unshift(name)
+  return kittens;
+}
